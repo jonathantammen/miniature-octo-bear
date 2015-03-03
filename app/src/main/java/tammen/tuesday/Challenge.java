b5a149d3ea071f85ce14734e0bd1e23d7987e6c1
Jonathan <jonathan.tammen@gmail.com>
2015-03-03 13:13:29 +1100

    Whatever happened before the crash

---
+++ app/src/main/java/tammen/tuesday/Challenge.java
@@ -0,0 +1,16 @@
+package tammen.tuesday;
+
+/**
+ * Created by tammen on 27/02/15.
+ */
+public class Challenge {
+    public Challenge() {
+
+    }
+
+    void calculatePoints() {
+
+    }
+
+    
+}
--- app/src/main/java/tammen/tuesday/MainActivity.java
+++ app/src/main/java/tammen/tuesday/MainActivity.java
@@ -1,5 +1,6 @@
 package tammen.tuesday;
 
+import android.support.v4.widget.SlidingPaneLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -12,6 +13,7 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SlidingPaneLayout sideBar = new SlidingPaneLayout(this);
     }
 
 
